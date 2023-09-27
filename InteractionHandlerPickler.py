a531e8bdbc956421292d747fa322cc93124a6bc4
Jake Richard <jake.richard126@gmail.com>
2023-09-27 12:57:44 -0400

    Forgot to remove .gitignore entry :(

---
+++ InteractionHandlerPickler.py
@@ -0,0 +1,8 @@
+import os
+import pickle
+from discord_lambda import InteractionHandler
+
+handler = InteractionHandler(command_dir="commands", app_id=os.environ.get('APP_ID'), public_key=os.environ.get('PUBLIC_KEY'), bot_token=os.environ.get('BOT_TOKEN'))
+
+with open("InteractionHandler.pickle", "wb") as f:
+    pickle.dump(handler, f, protocol=pickle.HIGHEST_PROTOCOL)
